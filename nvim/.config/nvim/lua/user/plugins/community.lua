5828b863aae466d33d3d5a721ed02d492c843534
puresyntax71 <34715246+puresyntax71@users.noreply.github.com>
2023-09-07 13:24:14 +0800

    Add atronvim.

---
+++ nvim/.config/nvim/lua/user/plugins/community.lua
@@ -0,0 +1,16 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of importing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {
+    "catppuccin",
+    opts = {
+      flavour = "macchiato",
+    },
+  },
+}
